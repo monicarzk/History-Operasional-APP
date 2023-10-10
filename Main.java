1dcb43a3d5dfb7a4c9ce7ee13a477352ed41fa23
monicarzk <monikarizki1933@gmail.com>
2023-10-11 00:16:13 +0700

    membuat scanner

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //membuat scanner untuk membaca input dari keyboard/user
+        Scanner scanner = new Scanner(System.in);
+    }
+}
