fd28aaede5ac5db898b8785e769e44f5ea5c3d67
monicarzk <monikarizki1933@gmail.com>
2023-10-11 00:31:38 +0700

    membuat parent class Keuangan.java lalu membuat enkapsulasi dan konstruktor

---
+++ Keuangan.java
@@ -0,0 +1,8 @@
+public class Keuangan {
+    private int saldo; //variabel saldo yang di deklarasikan menggunakan tipe data int (enkapsulasi)
+
+    //konstruktor mengatur nilai saldo sesuai dengan parameternya, parameternya yaitu saldoAwal
+    public Keuangan(int saldoAwal) {
+        this.saldo = saldoAwal;
+    }
+}
--- Main.java
+++ Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         //membuat scanner untuk membaca input dari keyboard/user
         Scanner scanner = new Scanner(System.in);
+
+        //membuat objek dengan saldo awal sebesar 100000000
+        Keuangan keuangan = new Keuangan(100000000);  // Saldo awal
     }
 }
